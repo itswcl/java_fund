3f4e4bcd540dd27925548a535c6a094d03403df7
weicheng lee <z82111712@gmail.com>
2021-11-28 14:49:22 -0800

    update first java program

---
+++ java_foundations/first_java_program/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Wei");
+        System.out.println("I'm 28 years old");
+        System.out.println("My hometown is Taiwan");
+    }
+}
