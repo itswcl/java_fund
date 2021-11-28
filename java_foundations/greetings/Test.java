7b5094c09e4036c15332d2ff62d60ddee3917ebf
weicheng lee <z82111712@gmail.com>
2021-11-28 14:33:21 -0800

    start java with hello world

---
+++ java_foundations/greetings/Test.java
@@ -0,0 +1,8 @@
+// file name must be same as class name "Test.java" > "Test"
+public class Test {
+    // exact method signature is always the launching point of app, MUST NEED
+    public static void main(String[] args) {
+        // code...
+        System.out.println("Hello World!");
+    }
+}
