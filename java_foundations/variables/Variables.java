2985c8288f4b88efc8de2c04345e25f9ba03835a
weicheng lee <z82111712@gmail.com>
2021-11-28 15:14:14 -0800

    java variables

---
+++ java_foundations/variables/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args){
+        // object Integer
+        int ourInt;
+        ourInt = 400;
+        // object Double
+        double pi = 3.14159265;
+        // object Boolean
+        boolean bool = true;
+        // object Character
+        char singleCharacter = 'A';
+
+        String multipleCharacter = "ABC";
+    }
+}
