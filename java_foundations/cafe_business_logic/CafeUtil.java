4ad5551357110b640fcfb161d2675318caf9b65a
weicheng lee <z82111712@gmail.com>
2021-11-29 13:58:52 -0800

    initial cafe biz

---
+++ java_foundations/cafe_business_logic/CafeUtil.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class CafeUtil {
+
+    // int getStreakGoal()
+
+
+    // printPriceChart
+
+
+    // double getOrderTotal(double[] prices)
+
+
+    // void displayMenu(ArrayList<String> menuItems)
+
+
+    // addCustomer(ArrayList<String>)
+
+
+}
