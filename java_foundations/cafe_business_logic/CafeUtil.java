979789e78f4b4c86e77e5cb96ea340638fea7641
weicheng lee <z82111712@gmail.com>
2021-11-29 14:15:20 -0800

    update test case for 1. streak goal and 2. order total

--- java_foundations/cafe_business_logic/CafeUtil.java
+++ java_foundations/cafe_business_logic/CafeUtil.java
@@ -5,12 +5,27 @@ import java.util.Arrays;
 public class CafeUtil {
 
     // int getStreakGoal()
-
-
-    // printPriceChart
-
+    public int getStreakGoal() {
+        int sum = 0;
+        int week = 0;
+        while (week <= 10) {
+            sum += week;
+            week++;
+        }
+        return sum;
+    }
 
     // double getOrderTotal(double[] prices)
+    public double getOrderTotal(double[] prices) {
+        double sum = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            sum += prices[i];
+        }
+        return sum;
+    }
+
+    // printPriceChart
 
 
     // void displayMenu(ArrayList<String> menuItems)
--- java_foundations/cafe_business_logic/TestCafe.java
+++ java_foundations/cafe_business_logic/TestCafe.java
@@ -5,19 +5,19 @@ public class TestCafe {
     public static void main(String[] args) {
 
     /*
-        You will need add 1 line to this file to create an instance
-        of the CafeUtil class.
+        You will need add 1 line to this file to create an instance of the CafeUtil class.
         Hint: it will need to correspond with the variable name used below..
     */
+    CafeUtil appTest = new CafeUtil();
 
         /* ============ App Test Cases ============= */
 
         System.out.println("\n----- Streak Goal Test -----");
         System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
 
-        // System.out.println("----- Order Total Test-----");
-        // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
-        // System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
+        System.out.println("----- Order ¡Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n",appTest.getOrderTotal(lineItems));
 
         // System.out.println("----- Display Menu Test-----");
 
