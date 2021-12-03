eb640a75a993e83a6b73c8e75feb521b8af5b05b
weicheng lee <z82111712@gmail.com>
2021-12-02 16:49:14 -0800

    update daikichiroute

---
+++ java_spring/intro/daikichiRoute/src/test/java/com/wei/daikichiRoute/DaikichiRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.daikichiRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
