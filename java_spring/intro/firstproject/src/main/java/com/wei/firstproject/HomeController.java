f9a7bca93b726d624430641ee7b9c89df70c89b5
weicheng lee <z82111712@gmail.com>
2021-12-02 19:11:52 -0800

    update first project with query field

--- java_spring/intro/firstproject/src/main/java/com/wei/firstproject/HomeController.java
+++ java_spring/intro/firstproject/src/main/java/com/wei/firstproject/HomeController.java
@@ -1,19 +1,32 @@
 package com.wei.firstproject;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@RequestMapping("/hello")
+//@RequestMapping("/hello")
 public class HomeController {
-	@RequestMapping("")
-	public String hello() {
-		return "Hello Worlqwed";
+	
+	@RequestMapping("/")
+	// function(annotation(query field, required=false)  String input)
+	public String index(@RequestParam(value="q", required=false) String searchQuery) {
+		if (searchQuery == null) {
+			return "You searched for: nothing";			
+		} else {
+			return "You searched for: " + searchQuery;			
+			
+		}
 	}
 	
-	@RequestMapping("/world")
-	public String world() {
-		return "Class level annotations are cool too!";
-	}
+//	@RequestMapping("")
+//	public String hello() {
+//		return "Hello Worlqwed";
+//	}
+//	
+//	@RequestMapping("/world")
+//	public String world() {
+//		return "Class level annotations are cool too!";
+//	}
 }
