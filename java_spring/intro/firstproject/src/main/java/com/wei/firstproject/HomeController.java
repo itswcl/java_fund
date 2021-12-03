d27894fa7cdf22752d0ea0868855cb43939c4c79
weicheng lee <z82111712@gmail.com>
2021-12-02 21:30:22 -0800

    pathvariable lect

--- java_spring/intro/firstproject/src/main/java/com/wei/firstproject/HomeController.java
+++ java_spring/intro/firstproject/src/main/java/com/wei/firstproject/HomeController.java
@@ -1,5 +1,6 @@
 package com.wei.firstproject;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,27 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	// function(annotation(query field, required=false)  String input)
-	public String index(@RequestParam(value="q", required=false) String searchQuery) {
+	public String index(
+			@RequestParam(value="q", required=false) String searchQuery
+			) 
+	{
 		if (searchQuery == null) {
 			return "You searched for: nothing";			
 		} else {
 			return "You searched for: " + searchQuery;			
 			
 		}
+	}
+	
+	@RequestMapping("/m/{track}/{module}/{lesson}")
+	public String showLesson(
+			// part of url as we set in request mapping
+			@PathVariable("track") String track,
+			@PathVariable("module") String module,
+			@PathVariable("lesson") String lesson
+			)
+	{
+		return track + " " + module + " " + lesson;
 	}
 	
 //	@RequestMapping("")
